fb1660e4c418651a14c57a05a1a1d9373cf6c3d1
Sristi Mazumder <sristimazumder2835@gmail.com>
2026-02-10 20:57:08 +0530

    Resolve merge conflicts - remove cache and settings files

--- websites/surveyfieldwork-nextjs/src/components/sections/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as Hero } from './Hero'
-export { default as Services } from './Services'
-export { default as About } from './About'
-export { default as Testimonials } from './Testimonials'
-export { default as Contact } from './Contact'
-export { default as CTA } from './CTA'
-export { default as EnterpriseSolutions } from './EnterpriseSolutions'
-export { default as JoinPanel } from './JoinPanel'
